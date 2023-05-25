d17902cf4cdd4ec5114278ff834623605186e108
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-05-25 14:55:33 +0200

    added condition to login

--- routes/login.js
+++ routes/login.js
@@ -12,22 +12,27 @@ router.post("/user/login", async (req, res) => {
     // console.log(email, password);
     const user = await User.findOne({ email: email });
     // console.log(user);
-    const salt = user.salt;
-    // console.log(salt);
-    const hash = user.hash;
-    // console.log(hash);
-    const newHash = SHA256(password + salt).toString(encBase64);
-    // console.log(newHash);
-    const response = {
-      id: user._id,
-      token: user.token,
-      account: user.account,
-    };
-    // console.log(response);
-    if (newHash === hash) {
-      res.status(200).json(response);
+    if (user) {
+      const salt = user.salt;
+      // console.log(salt);
+      const hash = user.hash;
+      // console.log(hash);
+      const newHash = SHA256(password + salt).toString(encBase64);
+      // console.log(newHash);
+      const response = {
+        id: user._id,
+        token: user.token,
+        account: user.account,
+      };
+      // console.log(response);
+
+      if (newHash === hash) {
+        res.status(200).json(response);
+      } else {
+        res.status(400).json({ message: "Unauthorized" });
+      }
     } else {
-      res.status(400).json({ message: "Unauthorized" });
+      res.status(400).json({ message: "No user found" });
     }
   } catch (error) {
     res.status(400).json({ message: error.message });
