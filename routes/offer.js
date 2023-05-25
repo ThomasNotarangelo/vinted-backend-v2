72f2d9e3c9ab5a30c8bd1daf3473c4c3263f4137
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-05-25 15:30:30 +0200

    added cloudinay & fileupload

--- routes/offer.js
+++ routes/offer.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const uid2 = require("uid2");
-const SHA256 = require("crypto-js/sha256");
-const encBase64 = require("crypto-js/enc-Base64");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 
 const Offer = require("../models/Offer");
 
-router.post("/offer/publish", (req, res) => {
+router.post("/offer/publish", fileUpload(), (req, res) => {
   try {
-    console.log("OK");
-    console.log(req.body);
+    console.log(req.files);
     res.status(200).json({ message: "OK" });
   } catch (error) {
     res.status(400).json({ message: error.message });
