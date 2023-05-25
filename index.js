a1988950aee35e304806a70f11d3a80e3a67c1fd
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-05-25 16:35:48 +0200

    route offer

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+require("dotenv").config();
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/vinted-v2");
+mongoose.connect(process.env.MONGODB_URI);
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
 
 const userRoutes = require("./routes/user");
 app.use(userRoutes);
@@ -14,6 +24,6 @@ app.all("*", (req, res) => {
   res.status(400).json({ message: "page not found" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server Started 🚀");
 });
--- routes/offer.js
+++ routes/offer.js
@@ -4,10 +4,30 @@ const fileUpload = require("express-fileupload");
 const cloudinary = require("cloudinary").v2;
 
 const Offer = require("../models/Offer");
+const convertToBase64 = (file) => {
+  return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
+};
 
-router.post("/offer/publish", fileUpload(), (req, res) => {
+router.post("/offer/publish", fileUpload(), async (req, res) => {
   try {
-    console.log(req.files);
+    // console.log(req.files);
+    // console.log(convertToBase64(req.files.picture));
+    const result = await cloudinary.uploader.upload(
+      convertToBase64(req.files.picture)
+    );
+    // console.log(result);
+    const { title, description, price, condition, city, brand, size, color } =
+      req.body;
+    // console.log(title, description, price, condition, city, brand, size, color);
+    const newOffer = new Offer({
+      product_name: title,
+      product_description: description,
+      product_price: price,
+      product_details: [condition, city, brand, size, color],
+      product_image: result,
+    });
+    console.log(newOffer);
+    // await newOffer.save();
     res.status(200).json({ message: "OK" });
   } catch (error) {
     res.status(400).json({ message: error.message });
