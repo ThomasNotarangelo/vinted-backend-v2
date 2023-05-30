3ad471a0d4a9526b13e969425aff89e28ce40bc9
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-05-30 10:50:10 +0200

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
+const cors = require("cors");
 const app = express();
-app.use(express.json());
 require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI);
 
